17106ab312e24c0052e85cfbabac7c2c662a38b4
李勇 <hzliyong@corp.netease.com>
2017-03-02 21:27:02 +0800

    新增RabbitMQ例子

--- rabbit/Producer.py
+++ rabbit/Producer.py
@@ -1,12 +1,21 @@
+# coding:utf-8
 import pika
-
+import time
 __author__ = 'hzliyong'
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue = 'hello')
+channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange = '', routing_key = 'hello', body = 'Hello World!')
+'''exchange为空字符串，可以允许发送给制定的queue，routing_key就是制定的queue名字'''
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+
+
+def send():
+    for i in range(10):
+        time.sleep(2)
+        channel.basic_publish(exchange='', routing_key='hello', body='Hello World!' + str(i))
+send()
 
 print("[x] Sent 'Hello World!'")
 connection.close()
--- rabbit/receive.py
+++ rabbit/receive.py
@@ -1,10 +1,10 @@
 import pika
-
 __author__ = 'hzliyong'
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue = 'hello')
+
 
 def callback(ch, method, properties, body):
     print("[x] Received %r" % body)
